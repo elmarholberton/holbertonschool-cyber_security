27c21a89e18594335d1efb4a437866e6745838c3
elmarholberton <9307@holbertonstudents.com>
2025-04-14 11:32:38 +0000

    Fix: 0-whois.sh updated to 2-line version

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-awk -F: '
-/Registrant|Admin|Tech/ && $2 {
-    gsub(/^[ \t]+|[ \t]+$/, "", $1);
-    gsub(/^[ \t]+|[ \t]+$/, "", $2);
-    print $1 "," $2;
-}
-' fake_whois.txt > "$1.csv"
+whois $1 | awk -F: '/Registrant|Admin|Tech/ && $2 {gsub(/^[ \t]+|[ \t]+$/, "", $1); gsub(/^[ \t]+|[ \t]+$/, "", $2); print $1 "," $2}' > $1.csv
 
