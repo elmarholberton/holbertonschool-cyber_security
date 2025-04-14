5d2c583e31757fd1c9c4e811514c7e77d4918952
elmarholberton <9307@holbertonstudents.com>
2025-04-14 11:59:21 +0000

    Fix: correct field parsing for email links

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F: '/Registrant|Admin|Tech/ && $2 {gsub(/^[ \t]+|[ \t]+$/, "", $1); gsub(/^[ \t]+|[ \t]+$/, "", $2); print $1 "," $2}' > $1.csv
+awk -F': ' '/(Registrant|Admin|Tech) (Organization|State\/Province|Country|Email)/ {gsub(/^[ \t]+|[ \t]+$/, "", $1); gsub(/^[ \t]+|[ \t]+$/, "", $2); print $1 "," $2}' fake_whois.txt > $1.csv
