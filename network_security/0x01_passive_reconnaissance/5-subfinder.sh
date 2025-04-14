9681b08eed7effa15e9ab15abc34d86014ad69ed
elmarholberton <9307@holbertonstudents.com>
2025-04-14 15:28:56 +0000

    Fix subfinder script to match checker format with -nW -oI

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-domain=$1
-subfinder -silent -d "$domain" | tee /dev/tty | while read sub; do
-    ip=$(dig +short "$sub" | head -n 1)
-    if [ -n "$ip" ]; then
-        echo "$sub,$ip" >> "${domain}.txt"
-    fi
-done
+subfinder -silent -d $1 -o $1.txt -nW -oI
