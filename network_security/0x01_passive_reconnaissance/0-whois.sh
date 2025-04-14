94185e9aa376b4dd4dbc63c9c7b7f18cd14cde9b
elmarholberton <9307@holbertonstudents.com>
2025-04-14 12:02:34 +0000

    Fix: dynamically read from domain.txt to match checker input

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk -F': ' '/(Registrant|Admin|Tech) (Organization|State\/Province|Country|Email)/ {gsub(/^[ \t]+|[ \t]+$/, "", $1); gsub(/^[ \t]+|[ \t]+$/, "", $2); print $1 "," $2}' fake_whois.txt > $1.csv
+awk -F': ' '/(Registrant|Admin|Tech) (Organization|State\/Province|Country|Email)/ {gsub(/^[ \t]+|[ \t]+$/, "", $1); gsub(/^[ \t]+|[ \t]+$/, "", $2); print $1 "," $2}' $1.txt > $1.csv
