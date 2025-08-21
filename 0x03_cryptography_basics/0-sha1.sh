7f6e273440f347c51046f80f70e6b9af9516cd66
elmarholberton <9307@holbertonstudents.com>
2025-08-21 04:50:15 -0400

    Fix 0-sha1.sh to checker-required form and add README.md

--- 0x03_cryptography_basics/0-sha1.sh
+++ 0x03_cryptography_basics/0-sha1.sh
@@ -1,12 +1,3 @@
-#!/usr/bin/env bash
-# 0-sha1.sh - Hash a given password using SHA-1 and save to 0_hash.txt
+#!/bin/bash
 
-# Check if an argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <password>"
-  exit 1
-fi
-
-# Hash the password with SHA-1, output only the hex digest, save to 0_hash.txt
-# -n prevents adding a newline to the input (important for exact hash)
-echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
+echo "$(echo -n "$1" | sha1sum)" > 0_hash.txt
