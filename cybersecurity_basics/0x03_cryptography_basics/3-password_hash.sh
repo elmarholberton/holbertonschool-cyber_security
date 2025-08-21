e688162f806a6ea465bd2230c9455113bc433ed0
elmarholberton <9307@holbertonstudents.com>
2025-08-21 08:32:40 -0400

    Cryptoghrapy Basics Task 3- Secure Password Hash

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# Generate a random 16-character salt
-SALT=$(openssl rand -hex 8)
-
-# Combine password ($1) with salt and hash using SHA-512
-echo -n "$1$SALT" | openssl sha512 > 3_hash.txt
+printf "%s%s" "$1" "$(openssl rand -hex 8)" | openssl sha512 | awk '{print $2}' > 3_hash.txt
