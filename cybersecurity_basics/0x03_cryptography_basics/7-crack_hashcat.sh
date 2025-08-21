cb56e10d58f65b4b0520b97568001ad647bb654d
elmarholberton <9307@holbertonstudents.com>
2025-08-21 10:16:58 -0400

    Fix task 7: hashcat straight attack; write cracked password to 7-password.txt

--- cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hashcat -m 0 -a 0 "$1" /usr/share/wordlists/rockyou.txt --quiet --show | awk -F: '{print $2}' > 7-password.txt
+hashcat -m 0 -a 0 "$1" /usr/share/wordlists/rockyou.txt --quiet --potfile-path ./hashcat.potfile >/dev/null 2>&1; hashcat -m 0 --show "$1" --potfile-path ./hashcat.potfile --quiet | awk -F: '{print $2}' > 7-password.txt
